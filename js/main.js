d522f3188b7df987e922568f66d35495f4e37c32
Miguel <miguel.martinez@utexas.edu>
2016-10-31 03:40:24 -0500

    feat: add JS code for scrolling on click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-/*Main JS*/
+$("#zero").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".zero").offset().top
+    }, 1000);
+});
+
+$("#one").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".one").offset().top
+    }, 1000);
+});
+
+$("#two").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".two").offset().top
+    }, 1000);
+});
+
+$("#three").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".three").offset().top
+    }, 1000);
+});
+
+$("#four").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".four").offset().top
+    }, 1000);
+});
+
+$("#five").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".five").offset().top
+    }, 1000);
+});
+
+$("#six").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".six").offset().top
+    }, 1000);
+});
